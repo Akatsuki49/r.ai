aebc50217fe18a2afa8144f243980f1b3f159fa0
Shubh Kanodia <pes1202100702@pesu.pes.edu>
2024-04-14 12:42:56 +0530

    frontend_changes

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,11 @@
 import 'package:arithmania_frontend/auth/login_screen.dart';
-import 'package:arithmania_frontend/screens/spendings_page.dart';
+import 'package:arithmania_frontend/screens/speech_recognition.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 import '/screens/chat_screen.dart';
 import '/screens/investment_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -15,8 +17,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Finance App'),
-        backgroundColor: Colors.blue[800],
+        backgroundColor: Colors.black,
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.logout),
@@ -32,28 +33,37 @@ class _HomeScreenState extends State<HomeScreen> {
         ],
       ),
       body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              Colors.blue[200]!,
-              Colors.blue[400]!,
-            ],
-          ),
-        ),
+        color: Colors.black,
         child: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               // Leave space for the Lottie file (animated mascot)
-              Container(
-                height: 200,
-                width: 200,
-                color: Colors.grey[300],
-                child: Image.asset('assets/images/mascot.png'),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Text('Welcome Siddhant!',
+                    style: GoogleFonts.roboto(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 44)),
               ),
-              SizedBox(height: 20),
+              SizedBox(height: 70),
+              Center(
+                child: Container(
+                  height: 200,
+                  width: 200,
+                  child: Image.asset('assets/images/mascot.png'),
+                ),
+              ),
+              SizedBox(height: 40),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: Text(
+                    'I’m r.ai and I’ll be your personal finance mentor. You can choose to chat with me via text or voice!',
+                    style:
+                        GoogleFonts.roboto(color: Colors.white, fontSize: 20)),
+              ),
+              SizedBox(height: 40),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
@@ -68,7 +78,7 @@ class _HomeScreenState extends State<HomeScreen> {
                       foregroundColor: Colors.indigo[800],
                     ),
                     child: Text(
-                      'Chat',
+                      'Use r.ai for Text',
                       style: TextStyle(
                         color: Colors.black,
                       ),
@@ -79,32 +89,15 @@ class _HomeScreenState extends State<HomeScreen> {
                       Navigator.push(
                         context,
                         MaterialPageRoute(
-                            builder: (context) => InvestmentScreen()),
+                            builder: (context) => SpeechRecognitionScreen()),
                       );
                     },
                     style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green[400],
                       foregroundColor: Colors.green[800],
                     ),
                     child: Text(
-                      'Investments',
-                      style: TextStyle(
-                        color: Colors.black,
-                      ),
-                    ),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => SpendingsPage()),
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      foregroundColor: Colors.green[800],
-                    ),
-                    child: Text(
-                      'Spendings',
+                      'Use r.ai for Voice',
                       style: TextStyle(
                         color: Colors.black,
                       ),
