a23995e9b86bf7ccadf40b81b8252672b09f8a4c
Sowmesh <sowmeshshailesh1234@gmail.com>
2024-04-14 05:53:19 +0530

    addded

--- lib/screens/spendings_page.dart
+++ lib/screens/spendings_page.dart
@@ -2,7 +2,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:arithmania_frontend/widgets/transaction_list.dart';
+import 'package:arithmania_frontend/widgets/transaction_list.dart';
 
 class SpendingsPage extends StatefulWidget {
   const SpendingsPage({Key? key}) : super(key: key);
@@ -103,7 +103,7 @@ class _SpendingsPageState extends State<SpendingsPage> {
                 child: Padding(
                   padding: const EdgeInsets.only(top: 20),
                   child: TransactionList(
-                    FirebaseAuth.instance.currentUser!.uid,
+                    userId: FirebaseAuth.instance.currentUser!.uid,
                   ),
                 ),
               ),
@@ -112,86 +112,6 @@ class _SpendingsPageState extends State<SpendingsPage> {
         ),
       ),
     );
-  }
-
-  Widget TransactionList(String userId) {
-    return StreamBuilder<QuerySnapshot>(
-      stream: FirebaseFirestore.instance
-          .collection('users')
-          .doc(userId)
-          .collection('transactions')
-          .orderBy('timestamp',
-              descending: true) // Order transactions by timestamp
-          .snapshots(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return CircularProgressIndicator();
-        }
-        if (snapshot.hasError) {
-          return Text('Error: ${snapshot.error}');
-        }
-        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-          return Text('No transactions found.');
-        }
-        return Padding(
-          padding: const EdgeInsets.only(top: 20),
-          child: SingleChildScrollView(
-            physics: BouncingScrollPhysics(),
-            child: Column(
-              children: snapshot.data!.docs.map((DocumentSnapshot document) {
-                Map<String, dynamic> data =
-                    document.data() as Map<String, dynamic>;
-                final double amount = data['amount'].toDouble();
-                final String type = data['type'];
-                final String category = data['category'];
-                final DateTime timestamp = data['timestamp'].toDate();
-                return Padding(
-                  padding: const EdgeInsets.only(bottom: 20),
-                  child: ListTile(
-                    tileColor: Color(0xff1D1D21),
-                    title: Text(
-                      category,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
-                    subtitle: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'Date: ${_formatDate(timestamp)}',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        Text(
-                          _formatAmount(amount, type),
-                          style: TextStyle(
-                            color:
-                                type == 'expense' ? Colors.red : Colors.green,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                );
-              }).toList(),
-            ),
-          ),
-        );
-      },
-    );
-  }
-
-  String _formatDate(DateTime date) {
-    return '${date.day}/${date.month}/${date.year}';
-  }
-
-  String _formatAmount(double amount, String type) {
-    if (type == 'expense') {
-      return '-₹${amount.toStringAsFixed(2)}'; // Expense amount in rupees
-    } else {
-      return '+₹${amount.toStringAsFixed(2)}'; // Income amount in rupees
-    }
   }
 
   Widget chotawidget() {
@@ -209,33 +129,36 @@ class _SpendingsPageState extends State<SpendingsPage> {
         return AlertDialog(
           title: Text('Add Transaction'),
           content: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                TextFormField(
-                  controller: _amountController,
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(labelText: 'Amount'),
-                ),
-                TextFormField(
-                  controller: _categoryController,
-                  decoration: InputDecoration(labelText: 'Category'),
-                ),
-                DropdownButton<String>(
-                  value: _type,
-                  onChanged: (String? newValue) {
-                    setState(() {
-                      _type = newValue!;
-                    });
-                  },
-                  items: <String>['expense', 'income'].map((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                ),
-              ],
+            child: Container(
+              width: double.maxFinite,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TextFormField(
+                    controller: _amountController,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(labelText: 'Amount'),
+                  ),
+                  TextFormField(
+                    controller: _categoryController,
+                    decoration: InputDecoration(labelText: 'Category'),
+                  ),
+                  DropdownButton<String>(
+                    value: _type,
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        _type = newValue!;
+                      });
+                    },
+                    items: <String>['expense', 'income'].map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                ],
+              ),
             ),
           ),
           actions: <Widget>[
