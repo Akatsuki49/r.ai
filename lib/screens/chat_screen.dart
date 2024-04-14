843fcd54b4b80ac3138387db8959aafa20a398e9
Sowmesh <sowmeshshailesh1234@gmail.com>
2024-04-14 14:13:49 +0530

    full end to end

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,9 +1,11 @@
 import 'dart:async';
+import 'dart:convert';
 import 'package:arithmania_frontend/screens/speech_recognition.dart';
 import 'package:arithmania_frontend/widgets/filled_text_field.dart';
 import 'package:arithmania_frontend/widgets/message_widget.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class Message {
   final String text;
@@ -144,19 +146,112 @@ class _ChatPageState extends State<ChatPage> {
     );
   }
 
+  // void _sendMessage() async {
+  //   // Ensure the message text is not empty
+  //   String messageText = _messageController.text.trim();
+  //   if (messageText.isNotEmpty) {
+  //     setState(() {
+  //       _isLoading = true;
+  //     });
+
+  //     // Get the current user's ID
+  //     String userId = FirebaseAuth.instance.currentUser!.uid;
+
+  //     try {
+  //       // Make the HTTP POST request
+  //       print({'question': messageText, 'user_id': userId});
+  //       var response = await http.post(
+  //         Uri.parse('https://b825-104-28-252-172.ngrok-free.app/query'),
+  //         body: json.encode({'question': messageText, 'user_id': userId}),
+  //         // headers: {'Content-Type': 'application/json'},
+  //       );
+  //       // Check if the request was successful (status code 200)
+  //       if (response.statusCode == 200) {
+  //         // Parse the response if necessary
+  //         var responseData = json.decode(response.body);
+
+  //         // Add the user message to the chat
+  //         _addUserMessage(messageText);
+
+  //         // Add the bot reply to the chat
+  //         _addBotReply(responseData['response']);
+  //       } else {
+  //         // Handle the error if the request was not successful
+  //         print('Request failed with status: ${response.statusCode}');
+  //       }
+  //     } catch (error) {
+  //       // Handle any errors that occur during the HTTP request
+  //       print('Error sending message: $error');
+  //     } finally {
+  //       setState(() {
+  //         _isLoading = false;
+  //       });
+  //     }
+
+  //     // Clear the message input field
+  //     _messageController.clear();
+  //   }
+  // }
   void _sendMessage() {
-    // print("UserID");
-    // print(FirebaseAuth.instance.currentUser!.uid);
+    // Ensure the message text is not empty
     String messageText = _messageController.text.trim();
     if (messageText.isNotEmpty) {
       setState(() {
         _isLoading = true;
       });
-      _addUserMessage(messageText);
-      _getBotReply(messageText);
+
+      // Prepare the request data
+      Map<String, dynamic> requestData = {
+        "question": messageText,
+        "user_id": FirebaseAuth.instance.currentUser!.uid,
+      };
+
+      // Send POST request to the backend endpoint
+      http
+          .post(
+        Uri.parse('https://b825-104-28-252-172.ngrok-free.app/query'),
+        body: requestData,
+      )
+          .then((response) {
+        if (response.statusCode == 200) {
+          // Parse the response if necessary
+          var responseData = json.decode(response.body);
+
+          // Add the user message to the chat
+          _addUserMessage(messageText);
+
+          // Add the bot reply to the chat
+          _addBotReply(responseData['response']);
+        } else {
+          print(
+              'Failed to send message to the backend. Status code: ${response.statusCode}');
+        }
+      }).catchError((error) {
+        print('Error sending message to the backend: $error');
+      }).whenComplete(() {
+        setState(() {
+          _isLoading = false;
+        });
+      });
+
+      // Clear the message input field
       _messageController.clear();
     }
   }
+
+  // void _sendMessage() {
+  //   // print("UserID");
+  //   // print(FirebaseAuth.instance.currentUser!.uid);
+  //   String messageText = _messageController.text.trim();
+  //   if (messageText.isNotEmpty) {
+  //     setState(() {
+  //       _isLoading = true;
+  //     });
+  //     _addUserMessage(messageText);
+  //     _getBotReply(messageText);
+  //     _messageController.clear();
+  //   }
+  // }
 
   void _addUserMessage(String messageText) {
     Message userMessage = Message(text: messageText, isUser: true);
--- lib/screens/spendings_page.dart
+++ lib/screens/spendings_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:arithmania_frontend/widgets/transaction_list.dart';
+import 'package:http/http.dart' as http;
 
 class SpendingsPage extends StatefulWidget {
   const SpendingsPage({Key? key}) : super(key: key);
@@ -15,110 +16,165 @@ class _SpendingsPageState extends State<SpendingsPage> {
   final TextEditingController _amountController = TextEditingController();
   final TextEditingController _categoryController = TextEditingController();
   String _type = "expense";
+  late String _balance;
+
+  @override
+  void initState() {
+    super.initState();
+    _updateBalance();
+  }
+
+  void _updateBalance() async {
+    String userId = FirebaseAuth.instance.currentUser!.uid;
+
+    FirebaseFirestore.instance
+        .collection('users')
+        .doc(userId)
+        .snapshots()
+        .listen((snapshot) {
+      if (snapshot.exists) {
+        // Accessing the balance value and updating the global variable
+        setState(() {
+          _balance = snapshot.data()?['balance']?.toString() ?? 'N/A';
+        });
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: Padding(
-        padding:
-            const EdgeInsets.only(top: 50, left: 20, right: 20, bottom: 10),
-        child: Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'My spendings',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 300),
-                child: Container(
-                  color: Colors.red,
-                  height: 60,
-                  width: 60,
-                ),
-              ),
-              Container(
-                color: Colors.white,
-                height: 150,
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                    color: Colors.white,
-                    height: 70,
-                    width: 70,
-                  ),
-                  Container(
-                    color: Colors.white,
-                    height: 70,
-                    width: 70,
-                  ),
-                  Container(
-                    color: Colors.white,
-                    height: 70,
-                    width: 70,
-                  ),
-                  Container(
-                    color: Colors.white,
-                    height: 70,
-                    width: 70,
-                  ),
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 20),
-                child: Center(
-                  child: Container(
-                    height: 50,
-                    width: 250,
-                    decoration: BoxDecoration(
-                      color: Color(0xff019254),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Center(
-                      child: GestureDetector(
-                        onTap: () => _showTransactionDialog(),
-                        child: Text(
-                          'Submit',
-                          style: TextStyle(color: Colors.white, fontSize: 20),
+    return _balance == null // Check if balance is null
+        ? Center(
+            child:
+                CircularProgressIndicator()) // Show CircularProgressIndicator while balance is null
+        : Builder(
+            builder: (context) => Scaffold(
+              backgroundColor: Colors.black,
+              body: Padding(
+                padding: const EdgeInsets.only(
+                    top: 50, left: 20, right: 20, bottom: 10),
+                child: Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'My spendings',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                    ),
+                      Padding(
+                          padding: const EdgeInsets.only(left: 300),
+                          child: Image.asset(
+                            'assets/images/mascot.png',
+                          )
+                          // child: Container(
+                          //   color: Colors.red,
+                          //   height: 60,
+                          //   width: 60,
+                          // ),
+                          ),
+                      Container(
+                        height: 150,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.white,
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(15),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                // crossAxisAlignment: CrossAxisAlignment.stretch,
+                                children: [
+                                  Image.asset('assets/images/cib_visa.png',
+                                      height: 50, width: 50),
+                                  SizedBox(width: 210),
+                                  Image.asset('assets/images/Group.png',
+                                      height: 50, width: 50),
+                                ],
+                              ),
+                              SizedBox(height: 20),
+                              Text('Available Balance',
+                                  style: TextStyle(fontSize: 15)),
+                              Text('₹ $_balance',
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold)),
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          chotawidget("25"),
+                          chotawidget("100"),
+                          chotawidget("500"),
+                          chotawidget("1000"),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 20),
+                        child: Center(
+                          child: Container(
+                            height: 50,
+                            width: 250,
+                            decoration: BoxDecoration(
+                              color: Color(0xff019254),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Center(
+                              child: GestureDetector(
+                                onTap: () => _showTransactionDialog(),
+                                child: Text(
+                                  'Submit',
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 20),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const Text('Recent',
+                          style: TextStyle(color: Colors.white, fontSize: 25)),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 20),
+                          child: TransactionList(
+                            userId: FirebaseAuth.instance.currentUser!.uid,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ),
-              const Text('Recent',
-                  style: TextStyle(color: Colors.white, fontSize: 25)),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 20),
-                  child: TransactionList(
-                    userId: FirebaseAuth.instance.currentUser!.uid,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
   }
 
-  Widget chotawidget() {
+  Widget chotawidget(String amt) {
     return Container(
-      color: Colors.white,
       height: 70,
       width: 70,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Center(
+        child: Text(
+          "₹" + amt,
+          style: TextStyle(color: Colors.black, fontSize: 17),
+        ),
+      ),
     );
   }
 
@@ -187,6 +243,7 @@ class _SpendingsPageState extends State<SpendingsPage> {
       "description": "Added manually",
       "timestamp": DateTime.now(),
     }).then((value) {
+      // Show success dialog
       showDialog(
         context: context,
         builder: (BuildContext context) {
@@ -205,6 +262,32 @@ class _SpendingsPageState extends State<SpendingsPage> {
         },
       );
       print('Transaction added successfully.');
+
+      // Prepare data to send to the backend endpoint
+      Map<String, dynamic> requestData = {
+        "user_id": FirebaseAuth.instance.currentUser!.uid,
+        "amount": _amountController.text,
+        "type": 'expense',
+        "category": _categoryController.text,
+        "description": "Added manually",
+      };
+
+      // Send POST request to the backend endpoint
+      http
+          .post(
+              Uri.parse(
+                  'https://b825-104-28-252-172.ngrok-free.app/update_transactions'),
+              body: requestData)
+          .then((response) {
+        if (response.statusCode == 200) {
+          print('Transaction data sent to the backend successfully.');
+        } else {
+          print(
+              'Failed to send transaction data to the backend. Status code: ${response.statusCode}');
+        }
+      }).catchError((error) {
+        print('Error sending transaction data to the backend: $error');
+      });
     }).catchError((error) {
       print('Error adding transaction: $error');
     });
